8a99171bf43a620a9ae8c03097e48632d33b025f
Henry Hunt <henhen7.24@gmail.com>
2021-09-22 01:55:42 -0500

    GPU memory frames working

--- PythonSimulation/lib/GPUKeplerSimulation.py
+++ PythonSimulation/lib/GPUKeplerSimulation.py
@@ -1,3 +1,4 @@
+import fractions
 import numpy as np
 import cupy as cp
 from math import comb, factorial, ceil, floor
@@ -35,9 +36,10 @@ def M_to_E(M, e):
     sins_of_M = cp.sin(cp.einsum("i,jk->ijk", cp.arange(0, CONST_ROWS+1), M))
     for n in range(1, CONST_ROWS):
         for k in range(0, int(cp.ceil(n/2))):
-            series += KEPLER_EQ_INV_CONSTANTS[n,
-                                              k]*cp.einsum("ij,i->ij", sins_of_M[n-2*k], cp.power(e, n))
+            series += KEPLER_EQ_INV_CONSTANTS[n, k]\
+                * cp.einsum("ij,i->ij", sins_of_M[n-2*k], cp.power(e, n))
     return M + series
+
 
 # The maximum GPU memory is required during the M_to_E step
 # The following arrays are in memory
@@ -51,13 +53,16 @@ def M_to_E(M, e):
 # So the total Memory is sizeof(float)*((CONST_ROWS+1 + 2)*size*steps + 3*size)
 FLOAT_SIZE = 8
 
+
 def _propagate_single_timestep_memory_(size):
-    return FLOAT_SIZE*(CONST_ROWS+3)*size
+    return FLOAT_SIZE*(CONST_ROWS+20)*size
+
 
 def _propagate_fixed_memory_(size):
     return FLOAT_SIZE*3*size
 
-def _propagate_single_gpu_memory_(belt, time_array, num_of_steps, initial_mean_anamoly=None):
+
+def _propagate_single_gpu_run_(belt, time_array, num_of_steps, initial_mean_anamoly=None):
 
     time_array = cp.array(time_array)
 
@@ -69,7 +74,7 @@ def _propagate_single_gpu_memory_(belt, time_array, num_of_steps, initial_mean_a
         initial_mean_anamoly = cp.array(initial_mean_anamoly)
 
     e = cp.array(belt.ecc.value)
-    
+
     a = cp.array(belt.a.to(u.km).value)
     T = (2*cp.pi*cp.power(cp.array(belt.a.to(u.km).value), 3.0/2.0) /
          cp.sqrt(belt.micro.to(u.km**3/u.s**2).value))
@@ -90,31 +95,51 @@ def _propagate_single_gpu_memory_(belt, time_array, num_of_steps, initial_mean_a
 
     before_ret = time.time()
 
-    print(" Obj Extract: ", before_E_calc - before_obj_extract, " E Calc: ", before_ret - before_E_calc)
+    print(" Obj Extract: ", before_E_calc - before_obj_extract,
+          " E Calc: ", before_ret - before_E_calc)
 
     return cp.asnumpy(radiuses)*u.km, cp.asnumpy(radial_momenta)*u.km/u.s, cp.asnumpy(final_mean_anamoly)*u.rad
 
 
 def propagate(belt, time_of_flight, timestep):
-    
+
     num_of_steps = int(time_of_flight.to(u.s)/timestep.to(u.s))
 
-    memory_gpu_size = cp.get_default_memory_pool().get_limit()
-    max_steps_in_one_gpu_memory = floor((memory_gpu_size - _propagate_fixed_memory_(belt.size))/_propagate_single_timestep_memory_(belt.size))
-    memory_frames = int(floor(num_of_steps/max_steps_in_one_gpu_memory))
+    mempool = cp.get_default_memory_pool()
+    memory_gpu_size = mempool.get_limit()
+    if memory_gpu_size == 0:
+        mempool.set_limit(fraction=0.8)
+        memory_gpu_size = mempool.get_limit()
+    max_steps_in_one_gpu_memory = floor(
+        (memory_gpu_size - _propagate_fixed_memory_(belt.size))/_propagate_single_timestep_memory_(belt.size))
+    memory_frames = int(ceil(num_of_steps/max_steps_in_one_gpu_memory))
+
+    print("MEM LIMIT: ", memory_gpu_size)
+
+    print("FIXED MEM: ", _propagate_fixed_memory_(belt.size),
+          "  STEP MEMORY: ", _propagate_single_timestep_memory_(belt.size))
+
+    print("Max Steps: ", (memory_gpu_size - _propagate_fixed_memory_(belt.size))/_propagate_single_timestep_memory_(belt.size),
+          "  Frames: ", memory_frames)
 
     time_array = np.linspace(0.0, time_of_flight, num_of_steps)
 
-    radiuses = np.zeros((belt.size, num_of_steps))
-    radial_momentum = np.zeros((belt.size, num_of_steps))
-    final_mean_anamoly = np.zeros(belt.size)
+    radiuses = np.zeros((belt.size, num_of_steps))*u.km
+    radial_momentum = np.zeros((belt.size, num_of_steps))*u.km/u.s
+    final_mean_anamoly = None
+
+    print("GPU Memory in use before frames: ", mempool.used_bytes())
 
     for i in range(0, memory_frames):
-        start_index = 
-        end_index = 
+        start_index = i*max_steps_in_one_gpu_memory
+        end_index = min((i+1)*max_steps_in_one_gpu_memory, num_of_steps)
+        print("Running propagation from ", start_index, " to ", end_index)
+        radiuses[:, start_index:end_index], radial_momentum[:, start_index:end_index], final_mean_anamoly = _propagate_single_gpu_run_(
+            belt, time_array[start_index:end_index], end_index-start_index, initial_mean_anamoly=final_mean_anamoly)
+        mempool.free_all_blocks()
+        print("After clearing: ", mempool.used_bytes())
 
-    time_array = cp.linspace(0.0, time_of_flight, num_of_steps)
-    
+    return radiuses, radial_momentum, final_mean_anamoly
 
 
 def average_scaler(quantity, start=None, end=None):
